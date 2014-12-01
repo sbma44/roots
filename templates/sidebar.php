086a005b3c1214a309fd7d8b2b905bd1a34c603d
Tom Lee <thomas.j.lee@gmail.com>
2014-12-01 12:50:01 -0500

    hide infobox link (dead) and add <ol><li> margin

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,5 +1,5 @@
 <p id="social-media-links">
-	<a href="/about"><i class="fa fa-info-circle"></i></a>
+	<!--<a href="/about"><i class="fa fa-info-circle"></i></a>-->
 	<a href="http://www.google.com/recaptcha/mailhide/d?k=015a0eLZE8OH-M1QVoN4rx_g==&amp;c=kKaXAEzEYVRYpmv1LcxheqoAdg6hbcroyKdCQEQ8aC0="><i class="fa fa-envelope"></i></a>
 	<a href="http://twitter.com/tjl"><i class="fa fa-twitter"></i></a>
 	<a href="http://flickr.com/photos/sbma44"><i class="fa fa-flickr"></i></a>
